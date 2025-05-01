11cb1c9a4035bb3dbdf29862be8b6b306116c8ac
Francesco <141887009+francesco-biondi@users.noreply.github.com>
2025-05-01 10:57:09 -0300

    Initial commit

---
+++ src/main/java/com/progra3/cafeteria_api/CafeteriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.progra3.cafeteria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeteriaApiApplication.class, args);
+	}
+
+}
