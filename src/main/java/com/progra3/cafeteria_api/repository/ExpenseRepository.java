dfe7d6bb2175a42c218dde449d13e079a37d5a9e
MaximoAndriola <maximoandriola2017@gmail.com>
2025-05-10 16:27:14 -0300

    feat (expenseRepository)
    add findDateBetween metod

--- src/main/java/com/progra3/cafeteria_api/repository/ExpenseRepository.java
+++ src/main/java/com/progra3/cafeteria_api/repository/ExpenseRepository.java
@@ -4,6 +4,10 @@ import com.progra3.cafeteria_api.model.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByDateBetween (LocalDateTime start, LocalDateTime end);
 }
