c596505efe1960addf6eb54e6365613275df53a0
Francesco <141887009+francesco-biondi@users.noreply.github.com>
2025-05-10 21:03:34 -0300

    Update GlobalHandler.java

--- src/main/java/com/progra3/cafeteria_api/exception/handler/GlobalHandler.java
+++ src/main/java/com/progra3/cafeteria_api/exception/handler/GlobalHandler.java
@@ -1,6 +1,10 @@
 package com.progra3.cafeteria_api.exception.handler;
 
 import com.progra3.cafeteria_api.exception.*;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
