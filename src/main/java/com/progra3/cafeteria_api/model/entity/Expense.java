e24e3786b98a85fa019bf4a110c174e7ee72a566
MaximoAndriola <maximoandriola2017@gmail.com>
2025-05-14 23:59:51 -0300

    fix(entities)
    refactor table names

--- src/main/java/com/progra3/cafeteria_api/model/entity/Expense.java
+++ src/main/java/com/progra3/cafeteria_api/model/entity/Expense.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "expense")
+@Table(name = "expenses")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
--- src/main/java/com/progra3/cafeteria_api/model/entity/Supplier.java
+++ src/main/java/com/progra3/cafeteria_api/model/entity/Supplier.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "supplier")
+@Table(name = "suppliers")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
