06353212724e958c534a32d5582cae9254a74e04
Matias Desimone <matiasdesimone10@gmail.com>
2025-06-13 22:06:10 -0300

    docs(AddressDTO): add AddressRequestDTO, AddressResponseDTO, AddressUpdateDTO documentation.

--- src/main/java/com/progra3/cafeteria_api/model/dto/AddressResponseDTO.java
+++ src/main/java/com/progra3/cafeteria_api/model/dto/AddressResponseDTO.java
@@ -1,11 +1,19 @@
 package com.progra3.cafeteria_api.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record AddressResponseDTO(
+        @Schema(description = "Street name of the address", example = "Av. Corrientes 1234")
         String street,
+
+        @Schema(description = "City of the address", example = "Buenos Aires")
         String city,
+
+        @Schema(description = "Province of the address", example = "Buenos Aires")
         String province,
+
+        @Schema(description = "Zip code of the address", example = "1406")
         String zipCode
 ) {}
