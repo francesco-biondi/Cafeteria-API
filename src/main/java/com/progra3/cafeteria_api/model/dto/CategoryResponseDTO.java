31b604c7fceb39b6c17ef52066664a2829cc262b
Matias Desimone <matiasdesimone10@gmail.com>
2025-06-13 22:08:39 -0300

    docs(CategoryDTO): add CategoryRequestDTO, CategoryResponseDTO documentation.

--- src/main/java/com/progra3/cafeteria_api/model/dto/CategoryResponseDTO.java
+++ src/main/java/com/progra3/cafeteria_api/model/dto/CategoryResponseDTO.java
@@ -1,12 +1,18 @@
 package com.progra3.cafeteria_api.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record CategoryResponseDTO(
+        @Schema(description = "Unique identifier of the category", example = "1")
         Long id,
+
+        @Schema(description = "Name of the category", example = "Beverages")
         String name,
+
+        @Schema(description = "List of products under this category")
         List<ProductResponseDTO> products
 ) { }
