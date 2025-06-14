840d6b168858b7950133b36e1f9b2c9b33748266
Matias Desimone <matiasdesimone10@gmail.com>
2025-06-13 22:14:18 -0300

    docs(ProductOptionDTO): add ProductOptionRequestDTO, ProductOptionResponseDTO documentation.

--- src/main/java/com/progra3/cafeteria_api/model/dto/ProductOptionResponseDTO.java
+++ src/main/java/com/progra3/cafeteria_api/model/dto/ProductOptionResponseDTO.java
@@ -1,9 +1,18 @@
 package com.progra3.cafeteria_api.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record ProductOptionResponseDTO(
+
+        @Schema(description = "Unique identifier of the product option", example = "1")
         Long id,
+
+        @Schema(description = "ID of the associated product", example = "10")
         Long productId,
+
+        @Schema(description = "Maximum quantity allowed for this product option", example = "5")
         Integer maxQuantity,
+
+        @Schema(description = "Additional price increase applied for this option", example = "2.5")
         Double priceIncrease
-) {
-}
+) {}
